cf877888d80468702ff48e37138d92a0c6e4c704
Al3ks1002 <al3ks1002@gmail.com>
2016-11-18 17:12:10 +0200

    added more string matching

---
+++ strings/string-matching/KMP.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class KMP {
+    public:
+        KMP(const string& text) : text_(text) {}
+
+        pair<int, vector<int>> GetMatchingIndexes(const string& pattern, int max_index_count) {
+            int count = 0;
+            vector<int> indexes;
+            indexes.reserve(max_index_count);
+
+            vector<int> prefix(pattern.size(), 0);
+            BuildPi(pattern, prefix);
+
+            int q = 0;
+            for (int i = 0; i < (int)text_.size(); i++) {
+                while (q && pattern[q] != text_[i]) {
+                    q = prefix[q - 1];
+                }
+
+                if (pattern[q] == text_[i]) {
+                    q++;
+                }
+
+                if (q == (int)pattern.size()) {
+                    if (++count <= max_index_count) {
+                        indexes.push_back(i - pattern.size() + 1);
+                    }
+                }
+            }
+
+            return {count, indexes};
+        }
+
+    private:
+        string text_;
+
+        void BuildPi(const string& pattern, vector<int>& prefix) {
+            int q = 0;
+            for (int i = 1; i < (int)pattern.size(); i++) {
+                while (q && pattern[q] != pattern[i]) {
+                    q = prefix[q - 1];
+                }
+
+                if (pattern[q] == pattern[i]) {
+                    q++;
+                }
+
+                prefix[i] = q;
+            }
+        }
+};
+
+int main() {
+    cin.sync_with_stdio(false);
+
+    string pattern;
+    cin >> pattern;
+
+    string text;
+    cin >> text;
+
+    KMP kmp(text);
+
+    pair<int, vector<int>> answer = kmp.GetMatchingIndexes(pattern, 1000);
+    cout << answer.first << '\n';
+    for (int it : answer.second) {
+        cout << it << " ";
+    }
+
+    return 0;
+}
---
+++ strings/string-matching/ZAlgorithm.cpp
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class ZAlgorithm {
+    public:
+        ZAlgorithm(const string& text) : text_(text) {}
+
+        vector<int> FindZ() {
+            vector<int> z(text_.size(), 0);
+            int left = -1;
+            int right = -1;
+            for (int i = 1; i < (int)text_.size(); i++) {
+                if (i > right) {
+                    int x = -1;
+                    int y = i - 1;
+                    while (y < (int)text_.size() - 1 && text_[x + 1] == text_[y + 1]) {
+                        x++;
+                        y++;
+                    }
+
+                    z[i] = x + 1;
+                    if (z[i]) {
+                        left = i;
+                        right = y;
+                    }
+                } else {
+                    int mirror = i - left;
+
+                    if (i + z[mirror] - 1 < right) {
+                        z[i] = z[mirror];
+                    } else {
+                        int x = right - i;
+                        int y = right;
+                        while (y < (int)text_.size() - 1 && text_[x + 1] == text_[y + 1]) {
+                            x++;
+                            y++;
+                        }
+
+                        z[i] = y - i + 1;
+                        left = i;
+                        right = y;
+                    }
+                }
+            }
+
+            return z;
+        }
+
+        pair<int, vector<int>> GetMatchingIndexes(const string& pattern, int max_index_count) {
+            text_ = pattern + '$' + text_;
+
+            int count = 0;
+            vector<int> indexes;
+            indexes.reserve(max_index_count);
+
+            vector<int> z = FindZ();
+            for (int i = 0; i < (int)z.size(); i++) {
+                if (z[i] == (int)pattern.size()) {
+                    if (++count <= max_index_count) {
+                        indexes.push_back(i - pattern.size() - 1);
+                    }
+                }
+            }
+
+            return {count, indexes};
+        }
+
+    private:
+        string text_;
+
+};
+
+int main() {
+    cin.sync_with_stdio(false);
+
+    string pattern;
+    cin >> pattern;
+
+    string text;
+    cin >> text;
+
+    ZAlgorithm zalgo(text);
+
+    pair<int, vector<int>> answer = zalgo.GetMatchingIndexes(pattern, 1000);
+    cout << answer.first << '\n';
+    for (int it : answer.second) {
+        cout << it << " ";
+    }
+
+    return 0;
+}
