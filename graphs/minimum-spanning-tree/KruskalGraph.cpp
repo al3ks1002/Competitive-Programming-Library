8ee21772fb7bc1b2382286d9a22bfe8cd0cf6a39
Al3ks1002 <al3ks1002@gmail.com>
2016-11-20 21:20:22 +0200

    added mst

---
+++ graphs/minimum-spanning-tree/KruskalGraph.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DisjointSet {
+    public:
+        DisjointSet(int size) : size_(size + 1) {
+            father_.resize(size_);
+            rank_.resize(size_);
+            for (int i = 0; i < size_; i++) {
+                father_[i] = i;
+                rank_[i] = 1;
+            }
+        }
+
+        int Find(int x) {
+            if (father_[x] != x) {
+                father_[x] = Find(father_[x]);
+            }
+            return father_[x];
+        }
+
+        // x and y must be roots
+        void Unite(int x, int y) {
+            if (rank_[x] < rank_[y]) {
+                father_[x] = y;
+            } else {
+                father_[y] = x;
+                if (rank_[x] == rank_[y]) {
+                    rank_[x]++;
+                }
+            }
+        }
+
+    private:
+        int size_;
+        vector<int> father_;
+        vector<int> rank_;
+};
+
+template<class T>
+class KruskalGraph {
+    public:
+        struct Edge {
+            int from_;
+            int to_;
+            T cost_;
+
+            Edge(int from , int to, T cost) : from_(from), to_(to), cost_(cost) {}
+
+            bool operator < (const Edge &other) const {
+                return cost_ < other.cost_;
+            }
+        };
+
+        KruskalGraph(int num_vertices) : num_vertices_(num_vertices) {}
+
+        void AddEdge(int from, int to, T cost) {
+            edges_.emplace_back(from, to, cost);
+        }
+
+        pair<T, vector<pair<int, int>>> GetMinimumSpanningTree() {
+            T total_cost = 0;
+            vector<pair<int, int>> spanning_tree;
+
+            DisjointSet disjoint_set(num_vertices_);
+
+            sort(edges_.begin(), edges_.end());
+            for (Edge& edge : edges_) {
+                int root_from = disjoint_set.Find(edge.from_);
+                int root_to = disjoint_set.Find(edge.to_);
+
+                if (root_from != root_to) {
+                    total_cost += edge.cost_;
+                    spanning_tree.push_back({edge.from_, edge.to_});
+                    disjoint_set.Unite(root_from, root_to);
+                }
+            }
+
+            return {total_cost, spanning_tree};
+        }
+
+    private:
+        int num_vertices_;
+        vector<Edge> edges_;
+};
+
+
+int main() {
+    cin.sync_with_stdio(false);
+
+    int n, m;
+    cin >> n >> m;
+
+    KruskalGraph<int> graph(n);
+    for (; m; m--) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        graph.AddEdge(x, y, z);
+    }
+
+    pair<int, vector<pair<int, int>>> ans = graph.GetMinimumSpanningTree();
+    cout << ans.first << '\n';
+
+    return 0;
+}
---
+++ graphs/minimum-spanning-tree/PrimGraph.cpp
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<class T>
+class PrimGraph {
+    public:
+        PrimGraph(int num_vertices) : num_vertices_(num_vertices) {
+            edges_.resize(num_vertices + 1);
+            distance_.resize(num_vertices + 1, kMaxCost);
+            father_.resize(num_vertices + 1, 0);
+            visited_.resize(num_vertices + 1, false);
+        }
+
+        void AddEdge(int from, int to, T cost) {
+            edges_[from].push_back({to, cost});
+            edges_[to].push_back({from, cost});
+        }
+
+        pair<T, vector<pair<int, int>>> GetMinimumSpanningTree() {
+            T total_cost = 0;
+            vector<pair<int, int>> spanning_tree;
+
+            ExtendVertex(1);
+            while (!queue.empty()) {
+                T current_cost = queue.top().first;
+                int current_vertex = queue.top().second;
+                queue.pop();
+
+                if (visited_[current_vertex]) {
+                    continue;
+                }
+
+                total_cost += current_cost;
+                spanning_tree.push_back({current_vertex, father_[current_vertex]});
+
+                ExtendVertex(current_vertex);
+            }
+
+            return {total_cost, spanning_tree};
+        }
+
+    private:
+        const int kMaxCost = numeric_limits<T>::max();
+        int num_vertices_;
+        vector<vector<pair<int, T>>> edges_;
+        vector<T> distance_;
+        vector<int> father_;
+        vector<bool> visited_;
+        priority_queue<pair<int, T>, vector<pair<int, T>>, greater<pair<int, T>>> queue;
+
+        void ExtendVertex(int vertex) {
+            visited_[vertex] = true;
+            for (auto& edge : edges_[vertex]) {
+                int neighbour = edge.first;
+                T cost = edge.second;
+
+                if (!visited_[neighbour] && cost < distance_[neighbour]) {
+                    distance_[neighbour] = cost;
+                    father_[neighbour] = vertex;
+                    queue.push({distance_[neighbour], neighbour});
+                }
+            }
+        }
+};
+
+int main() {
+    cin.sync_with_stdio(false);
+
+    int n, m;
+    cin >> n >> m;
+
+    PrimGraph<int> graph(n);
+    for (; m; m--) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        graph.AddEdge(x, y, z);
+    }
+
+    pair<int, vector<pair<int, int>>> ans = graph.GetMinimumSpanningTree();
+    cout << ans.first << '\n';
+
+    return 0;
+}
