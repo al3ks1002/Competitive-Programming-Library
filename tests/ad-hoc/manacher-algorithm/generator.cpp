faff7a08c0e125890d175958456ca7b011f51029
Al3ks1002 <al3ks1002@gmail.com>
2016-12-05 20:40:50 +0200

    added manacher

---
+++ tests/ad-hoc/manacher-algorithm/generator.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+const int kMaxN = 1e5;
+
+int main() {
+    srand(time(0));
+
+    for (int i = 1; i <= 10; i++) {
+        string test = "test";
+        stringstream my_stream;
+        string in = ".in";
+        my_stream << test << i << in;
+        string str = my_stream.str();
+        ofstream cout(str);
+
+        int n;
+        if (i <= 2) {
+            n = 10;
+        } else if (i <= 7) {
+            n = rand() % kMaxN;
+        } else {
+            n = kMaxN;
+        }
+
+        for (int i = 0; i < n; i++) {
+            cout << (char)(rand() % ('z' - 'a' + 1) + 'a');
+        }
+    }
+
+    return 0;
+}
