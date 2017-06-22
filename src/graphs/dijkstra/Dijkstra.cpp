6ffb4dbf76a45c286544151353d3342c85699b22
Al3ks1002 <al3ks1002@gmail.com>
2017-06-22 09:11:40 +0300

    fixed T bug

--- src/graphs/dijkstra/Dijkstra.cpp
+++ src/graphs/dijkstra/Dijkstra.cpp
@@ -55,7 +55,7 @@ class DijkstraGraph {
         }
 
     private:
-        static const int kMaxInf = numeric_limits<T>::max();
+        static const T kMaxInf = numeric_limits<T>::max();
         const int num_vertices_;
         vector<vector<pair<int, T>>> edges_;
 };
